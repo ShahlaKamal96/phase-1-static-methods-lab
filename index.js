9f233b49daaf691b9567bee9de58e3645344d897
ShahlaKamal96 <shahla.kamal19@gmail.com>
2023-01-23 13:43:04 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(str) {
+    return str.replace(/[^\w-\s']/gi, '');
+  }
+
+  static titleize(str) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0 || !exceptions.includes(words[i].toLowerCase())) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+      }
+    }
+    return words.join(" ");
+  }
+
 }
